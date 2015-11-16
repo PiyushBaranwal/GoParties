73243e510be6bdb7207750fd608b658e9e557623
GoParties <goparties@gopartiess-mac-mini.local>
2015-11-16 11:03:59 +0530

    Initial Commit

---
+++ GoParties/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoParties
+//
+//  Created by GoParties on 16/11/15.
+//  Copyright © 2015 GoParties. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
